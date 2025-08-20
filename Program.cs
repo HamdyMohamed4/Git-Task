cbe167623b51f8288bc6421be6cd2d406b7ee4df
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2025-08-20 13:38:07 +0300

    Improve startup message in Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ class Program
 {
     static void Main()
     {
+Console.WriteLine("Starting ITI Git Lab...");
         // Create instances of the classes
         HelloSwift hw = new HelloSwift();
         HelloITI hi = new HelloITI();

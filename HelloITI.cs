d5dfa775d463ca10748b22d7e3041869aba56827
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2025-08-20 13:32:02 +0300

    Add PrintHelloGit method to HelloITI class

--- HelloITI.cs
+++ HelloITI.cs
@@ -3,6 +3,10 @@ using System;
 
 public class HelloITI
 {
+public void PrintHelloGit()
+{
+    Console.WriteLine("Hello Git");
+}
     public void PrintHelloITI()
     {
         Console.WriteLine("Hello ITI");
